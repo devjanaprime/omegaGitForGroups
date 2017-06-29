0a3cd9489c8184f10b5ed8b2d9f231fea97028e4
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-06-29 12:32:13 -0500

    feature done

--- example.js
+++ example.js
@@ -1,5 +1,12 @@
-console.log( 'js' );
+console.log('js');
 
-function addSomeone(){
-  console.log( 'in addSomeone' );
+function addSomeone() {
+  console.log('in addSomeone');
+  var randInt = Math.floor(Math.random() * 100);
+  console.log(randInt);
+  var hereString = '';
+  for (var i = 0; i < randInt; i++) {
+    hereString += ' here';
+  }
+  console.log('Hogan was' + hereString);
 }
